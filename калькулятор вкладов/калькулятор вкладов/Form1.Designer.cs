9da9f1db3619bdc90f809623aa38cf257ea2664f
samoylo7725 <samolenko573@gmail.com>
2025-04-26 01:50:08 +0300

    Add files via upload
    школьный проект ученика 10 а

---
+++ калькулятор вкладов/калькулятор вкладов/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace калькулятор_вкладов
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox5 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "100000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "сбербанк",
+            "тинькофф"});
+            this.checkedListBox1.Location = new System.Drawing.Point(27, 133);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(136, 139);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "сбервклад",
+            "вклад\'лучший %\'"});
+            this.checkedListBox2.Location = new System.Drawing.Point(178, 133);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(148, 64);
+            this.checkedListBox2.TabIndex = 4;
+            this.checkedListBox2.Visible = false;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged);
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "пополняемый",
+            "не пополняемый"});
+            this.checkedListBox3.Location = new System.Drawing.Point(178, 208);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(148, 64);
+            this.checkedListBox3.TabIndex = 5;
+            this.checkedListBox3.Visible = false;
+            this.checkedListBox3.SelectedIndexChanged += new System.EventHandler(this.checkedListBox3_SelectedIndexChanged);
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Items.AddRange(new object[] {
+            "3 месяца",
+            "6 месяцев",
+            "12 месяцев"});
+            this.checkedListBox4.Location = new System.Drawing.Point(348, 208);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(120, 64);
+            this.checkedListBox4.TabIndex = 6;
+            this.checkedListBox4.Visible = false;
+            this.checkedListBox4.SelectedIndexChanged += new System.EventHandler(this.checkedListBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "итог";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "x";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox5
+            // 
+            this.checkedListBox5.FormattingEnabled = true;
+            this.checkedListBox5.Items.AddRange(new object[] {
+            "обычный",
+            "есть сбер прайм",
+            "траты от 30 тысяч р"});
+            this.checkedListBox5.Location = new System.Drawing.Point(348, 135);
+            this.checkedListBox5.Name = "checkedListBox5";
+            this.checkedListBox5.Size = new System.Drawing.Size(258, 64);
+            this.checkedListBox5.TabIndex = 9;
+            this.checkedListBox5.Visible = false;
+            this.checkedListBox5.SelectedIndexChanged += new System.EventHandler(this.checkedListBox5_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 19);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(378, 52);
+            this.label2.TabIndex = 11;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "рублей";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 445);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox4);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ калькулятор вкладов/калькулятор вкладов/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace калькулятор_вкладов
+{
+     public partial class Form1 : Form
+    {
+        public static class Globals //создаётся общий класс к которому будут присвоены переменные//
+        {
+            public static int sum=100000,bank=0,srok=0,ttype=0,sbertype=0,sberpodtype=0;//используемые глобалные переменные//
+        }
+        public Form1()//создание формы//
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)//проверка на изменение элемента выбора//
+        {
+            //выбор банка с появлением зависимых элементов//
+            if (checkedListBox1.SelectedIndex == 0)
+            {
+                checkedListBox3.Visible = false;//изменение не видимости зависимого элемента//
+                checkedListBox2.Visible = true; //изменение видимости зависимого элемента//
+                Globals.bank = 0;//изменение глобальной переменной//
+            }
+            else if (checkedListBox1.SelectedIndex == 1)
+            {
+                checkedListBox5.Visible = false;//изменение не видимости зависимого элемента//
+                checkedListBox3.Visible = true; //изменение видимости зависимого элемента//
+                checkedListBox2.Visible = false;//изменение не видимости зависимого элемента//
+                Globals.bank = 1;//изменение глобальной переменной//
+            }
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)//проверка на изменение элемента выбора//
+        {
+            //выбор типа вклада//
+            checkedListBox4.Visible = true;
+            
+            if (checkedListBox2.SelectedIndex == 0)
+            {
+                checkedListBox5.Visible = true;
+                Globals.sbertype = 0;
+            }
+            else if (checkedListBox2.SelectedIndex == 1)
+            {
+                checkedListBox5.Visible =false;
+                Globals.sbertype = 1;
+            }
+        }
+
+        private void checkedListBox3_SelectedIndexChanged(object sender, EventArgs e)//проверка на изменение элемента выбора//
+        {
+            //выбор типа вклада//
+            checkedListBox4.Visible = true;
+            if (checkedListBox3.SelectedIndex == 0)
+            {
+                checkedListBox5.Visible = false;
+                Globals.ttype = 2;
+            }
+            else if (checkedListBox3.SelectedIndex == 1)
+            {
+                checkedListBox5.Visible = false;
+                Globals.ttype = 3;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Globals.sum = Convert.ToInt32(textBox1.Text);//присваивание переменной суммы//
+        }
+
+        private void checkedListBox4_SelectedIndexChanged(object sender, EventArgs e)//проверка на изменение элемента выбора//
+        {
+            //выбор срока вклада//
+            if (checkedListBox4.SelectedIndex == 0)//3 месяца//
+            {
+                Globals.srok = 0;
+            }
+            else if (checkedListBox4.SelectedIndex == 1)//6 месяцев//
+            {
+                Globals.srok = 1;
+            }
+            else if (checkedListBox4.SelectedIndex == 2)//12 месяцев//
+            {
+                Globals.srok = 2;
+            }
+        }
+        private void checkedListBox5_SelectedIndexChanged(object sender, EventArgs e)//проверка на изменение элемента выбора//
+        {
+            //выбор подтипа вклада сбербанка//
+            if (checkedListBox4.SelectedIndex == 0)
+            {
+                Globals.sberpodtype = 0;
+            }
+            else if (checkedListBox4.SelectedIndex == 1)
+            {
+                Globals.sberpodtype = 1;
+            }
+            else if (checkedListBox4.SelectedIndex == 2)
+            {
+                Globals.sberpodtype = 2;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//нажате кнопки активирует пересчёт//
+        {
+            //глобальная проверка//
+            if (Globals.bank == 0)//банк//
+            {
+                if (Globals.sbertype == 0)//тип вклада//
+                {
+                    if (Globals.srok == 0)//срок вклада//
+                    {
+                        if (Globals.sberpodtype == 0)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 / 4), 1));//общий расчёт с округлением до сотых//
+                        }
+                        else if (Globals.sberpodtype == 1)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 / 4), 1));//общий расчёт с округлением до сотых//
+                        }
+                        else if (Globals.sberpodtype == 2)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 / 4), 1));//общий расчёт с округлением до сотых//
+                        }
+
+                    }
+                    else if (Globals.srok == 1)//срок вклада//
+                    {
+                        if (Globals.sberpodtype == 0)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 / 2), 2));//общий расчёт с округлением до сотых//
+                        }
+                        else if (Globals.sberpodtype == 1)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 / 2), 2));//общий расчёт с округлением до сотых//
+                        }
+                        else if (Globals.sberpodtype == 2)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 / 2), 2));//общий расчёт с округлением до сотых//
+                        }
+                        
+                    }
+                    else if (Globals.srok == 2)//срок вклада//
+                    {
+                        if (Globals.sberpodtype == 0)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 ), 2));//общий расчёт с округлением до сотых//
+                        }
+                        else if (Globals.sberpodtype == 1)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 ), 2));//общий расчёт с округлением до сотых//
+                        }
+                        else if (Globals.sberpodtype == 2)//подтип вклада//
+                        {
+                            label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.155 ), 2));//общий расчёт с округлением до сотых//
+                        }
+
+                    }
+                }
+                else if (Globals.sbertype == 1)//тип вклада//
+                {
+                    if (Globals.srok == 0)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.19 / 4), 2));//общий расчёт с округлением до сотых//
+                    }
+                    else if (Globals.srok == 1)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.19 / 2), 2));//общий расчёт с округлением до сотых//
+                    }
+                    else if (Globals.srok == 2)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.19 ), 2));//общий расчёт с округлением до сотых//
+                    }
+                }
+            }
+            else if (Globals.bank == 1)//банк//
+            {
+                if (Globals.ttype == 2)//тип вклада//
+                {
+                    if (Globals.srok == 0)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.1724 / 4), 2));//общий расчёт с округлением до сотых//
+                    }
+                    else if (Globals.srok == 1)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.1761 / 2), 2));//общий расчёт с округлением до сотых//
+                    }
+                    else if (Globals.srok == 2)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.138), 2));//общий расчёт с округлением до сотых//
+                    }
+             
+                }
+                else if (Globals.ttype == 3)//тип вклада//
+                {
+                    if (Globals.srok == 0)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.193 / 4), 2));//общий расчёт с округлением до сотых//
+                    }
+                    else if (Globals.srok == 1)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.1922 / 2), 2));//общий расчёт с округлением до сотых//
+                    }
+                    else if (Globals.srok == 2)//срок вклада//
+                    {
+                        label1.Text = Convert.ToString(Math.Round(Globals.sum * (1 + 0.1838), 2));//общий расчёт с округлением до сотых//
+                    }
+                    
+                }
+            }
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(0);//стирание итога//
+        }
+
+        private void button3_Click(object sender, EventArgs e)//информационная кнопка//
+        {
+            if (label2.Visible == false)
+            {
+                label2.Visible = true;//появлене информации//
+            }
+            else
+            {
+                label2.Visible = false;
+            }
+        }
+
+        
+    }
+}
